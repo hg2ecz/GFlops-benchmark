d8690c352ec0e8845079587a7446b4f232673787
Zsolt Krüpl <hg2ecz@ham.hu>
2020-01-05 17:10:59 +0100

    +x86

--- ARM-benchmark/Rotate-MEM-MLA/rotate.h
+++ ARM-benchmark/Rotate-MEM-MLA/rotate.h
@@ -1,8 +1,14 @@
-#include <arm_neon.h>
+#if defined(__arm__) || defined(__aarch64__)
+  #include <arm_neon.h>
+  typedef float32x4_t VECTYPE;
+#else
+  #include <smmintrin.h>
+  typedef __v4sf VECTYPE;
+#endif
 
 struct _cf32x4_t {
-    float32x4_t re;
-    float32x4_t im;
+    VECTYPE re;
+    VECTYPE im;
 };
 
 struct _cf32x4_t rotate(const struct _cf32x4_t *base, const struct _cf32x4_t *phase);
--- ARM-benchmark/Rotate-REG/rotate.c
+++ ARM-benchmark/Rotate-REG/rotate.c
@@ -2,7 +2,7 @@
 
 struct _cf32x4_t rotate(struct _cf32x4_t base, struct _cf32x4_t phase) {
     for (int i=0; i<1000*1000; i++) {
-        float32x4_t base_tmpre = base.re;
+        VECTYPE base_tmpre = base.re;
         base.re = base_tmpre * phase.re - base.im * phase.im;
         base.im = base_tmpre * phase.im + base.im * phase.re;
     }
